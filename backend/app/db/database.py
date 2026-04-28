898715526b499d253b2a8c9f273a1c3fa87966cc
Myan Gupta <181651739+Myan17@users.noreply.github.com>
2026-04-27 22:18:02 -0500

    chore: optimize db connection pool for high concurrency

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -3,7 +3,12 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, echo=True)
+engine = create_engine(
+    settings.DATABASE_URL, 
+    echo=False, 
+    pool_size=20, 
+    max_overflow=20
+)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
